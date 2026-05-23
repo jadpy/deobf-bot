0d71398ebc5530112017e11c7e12b2f522b1178e
jadpy <165412492+jadpy@users.noreply.github.com>
2026-05-24 06:09:29 +0900

    Upload deobfuscated file: prometheus.obfuscated.lua

---
+++ contents/1779570568749/prometheus.obfuscated.lua
@@ -0,0 +1,4 @@
+local enable1 = true
+local v1 = {}
+local tbl = setmetatable({}, { ["__index"] = v1, ["__metatable"] = nil })
+print(GAME[tbl[Z("\xec?\n\xa8\xe3S\xc2", 1436905945681)]] == 1234567890)
