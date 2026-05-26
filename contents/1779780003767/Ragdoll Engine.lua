7670c907f5b957c02d37847535225357f76359cb
jadpy <165412492+jadpy@users.noreply.github.com>
2026-05-26 16:20:04 +0900

    Upload deobfuscated file: Ragdoll%20Engine.lua

---
+++ contents/1779780003767/Ragdoll Engine.lua
@@ -0,0 +1,5 @@
+fenv._cGTGVmmXbCVv = "This file was protected with MoonSec V3"
+wait()
+fenv.MoonSec_StringsHiddenAttr = true
+getgenv().hiddenfling = false
+game:GetService("ReplicatedStorage")["FindFirstChild"](game:GetService("ReplicatedStorage"), "SigmaNil")
