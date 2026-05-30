8b3afebe6d4d2662bb040a0d9322f35c46cff789
jadpy <165412492+jadpy@users.noreply.github.com>
2026-05-30 09:11:20 +0900

    Upload deobfuscated file: raw.lua

---
+++ contents/1780099879969/raw.lua
@@ -0,0 +1,83 @@
+local function function1(...) Env[T(9775)](T(10705)) end
+
+local v18 = Env[T(10424)](function(...)
+    local toggle4 = true
+    e[k] = toggle4 
+end)
+local toggle3 = v18 and false
+local func2 = Env[T(10713)][T(10379)]
+local v17 = Env[T(9730)][T(10313)]
+local v10 = Env[T(9730)] and Env[T(9730)][T(10173)] or Env[T(10173)]
+Env[T(10745)](Env[T(9843)][T(10116)](Env[T(10058)](({
+    Env[T(10424)](function(...)
+        local v13 = 2947015
+        local v14 = T(10210)
+        local v15 = 16367008
+        local v16 = v14 ^ v15
+        local v122 = v15 - v16
+        v15 = T(10761)
+        v122 = v122
+        v122 = v15 / v122
+        return v122 
+    end)
+})[2]), T(10119))())
+for index = 1, func2(3, 65) do
+    local v11 = Env[T(10713)][T(10379)](1, 100)
+    local toggle2 = func2(1, 2) == 1
+    s[T(10439)](s, T(10119), T(9943) .. Env[T(10058)](func2(0, 10000)) .. T(9943))
+    {
+        Env[T(10424)](function(...)
+            local toggle1 = func2(1, 2) == 1 or index == func2(3, 65)
+            if toggle1 then
+                toggle1 = T(10745)
+                local func3 = Env[toggle1]
+                toggle1 = func3(Env[T(9843)][T(10116)](Env[T(10058)](({
+                    Env[T(10424)](function(...)
+                        local v32 = 5776698
+                        local v22 = v32 - T(10557) ^ 13323789
+                        v32 = T(9734)
+                        local v4 = v22
+                        v22 = v32 / v4
+                        return v22 
+                    end)
+                })[2]), T(10119))())
+                toggle1 = true and Env[T(10745)](r2(Env[T(10058)](({
+                    Env[T(10424)](function(...)
+                        local v6 = 2947015
+                        local v7 = T(10210)
+                        local v8 = 16367008
+                        local v9 = v7 ^ v8
+                        local v5 = v8 - v9
+                        v8 = T(10761)
+                        v5 = v5
+                        v5 = v8 / v5
+                        return v5 
+                    end)
+                })[2]), T(10119))()) == toggle1
+                r1 = toggle1
+            end
+            local func = toggle2
+            if func then
+                func = Env[T(9775)]
+                func(e[H], 0)
+            end
+            func = {}
+            local list2 = v10
+            for index2 = 1, v11 do
+                func = func2
+                list2[index2] = func(0, 255) 
+            end
+            func = func2(1, v11)
+            list2[func] = func2(0, 255)
+            func = v10
+            return func(list2) 
+        end)
+    } 
+end
+if true and 0 == 0 then
+    Env[T(10713)][T(10028)]
+    local list1 = {}
+    local v12 = 0
+    v12 = Env[T(9730)][T(10463)](list1, Env[T(10713)][T(10379)](1, #list1))
+    Env[T(9843)][T(10261)](v12 - 1)
+end
