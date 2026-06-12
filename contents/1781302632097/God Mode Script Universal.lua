8d04befe4156b175e528cfdfc80c7daa6130b61c
jadpy <165412492+jadpy@users.noreply.github.com>
2026-06-13 07:17:12 +0900

    Upload deobfuscated file: God%20Mode%20Script%20Universal.lua

---
+++ contents/1781302632097/God Mode Script Universal.lua
@@ -0,0 +1,61 @@
+local instFrame2 = Instance.new("Frame")
+local instSound6 = Instance.new("Sound")
+game.Players.LocalPlayer.WaitForChild(game.Players.LocalPlayer, "PlayerGui")
+Instance.new("ScreenGui").ZIndexBehavior = {
+  ["_enumType"] = Instance,
+  ["_name"] = "Sibling",
+  ["_value"] = 0
+}
+instScreenGui1.ResetOnSpawn = false
+instFrame2.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+instFrame2.BorderSizePixel = 0
+instFrame2.Position = UDim2.new(0.38, 0, 0.3, 0)
+instFrame2.Size = UDim2.new(0, 220, 0, 120)
+instFrame2.Active = true
+instFrame2.Draggable = true
+instTextLabel3.BackgroundColor3 = Color3.fromRGB(25, 15, 25)
+instTextLabel3.BorderSizePixel = 0
+instTextLabel3.Size = UDim2.new(1, 0, 0, 25)
+Instance.new("TextLabel").Font = {
+  ["_enumType"] = Instance,
+  ["_name"] = "SourceSansBold",
+  ["_value"] = 0
+}
+instTextLabel3.Text = "God Mode Script Universal"
+instTextLabel3.TextColor3 = Color3.fromRGB(255, 255, 255)
+instTextLabel3.TextSize = 18
+instTextButton4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+instTextButton4.BorderSizePixel = 0
+instTextButton4.Position = UDim2.new(0.5, -77.5, 0.5, -22.5)
+instTextButton4.Size = UDim2.new(0, 155, 0, 45)
+Instance.new("TextButton").Font = {
+  ["_enumType"] = Instance,
+  ["_name"] = "SourceSansItalic",
+  ["_value"] = 0
+}
+instTextButton4.Text = "God Mode: OFF"
+instTextButton4.TextColor3 = Color3.fromRGB(0, 0, 0)
+instTextButton4.TextSize = 18
+instTextLabel5.BackgroundColor3 = Color3.fromRGB(25, 15, 25)
+instTextLabel5.BorderSizePixel = 0
+instTextLabel5.Position = UDim2.new(0, 0, 1, -20)
+instTextLabel5.Size = UDim2.new(1, 0, 0, 20)
+Instance.new("TextLabel").Font = {
+  ["_enumType"] = Instance,
+  ["_name"] = "SourceSans",
+  ["_value"] = 0
+}
+instTextLabel5.Text = "Made by @botreal1 on TikTok"
+instTextLabel5.TextColor3 = Color3.fromRGB(255, 255, 255)
+instTextLabel5.TextSize = 18
+instSound6.SoundId = "rbxassetid://12221967"
+instSound6.Volume = 1.1
+local cb10 = function(Instance_cb10arg1, Instance_cb10arg2, Instance_cb10arg3)
+Instance.new("LocalScript")
+game:GetService("Players")
+        -- Error executing callback: [string "script"]:823: attempt to index nil with 'MouseButton1Click'
+end
+coroutine.resume(coroutine.wrap(cb10), )
+Instance.new("LocalScript")
+game:GetService("Players")
+error("[string \"script\"]:823: attempt to index nil with 'MouseButton1Click'")
