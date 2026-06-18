d417f9490610710d4b012e06621ba253b06b3d9e
jadpy <165412492+jadpy@users.noreply.github.com>
2026-06-18 21:32:22 +0900

    Upload deobfuscated file: The_Chosen_One_Lite.lua

---
+++ contents/1781785941218/The_Chosen_One_Lite.lua
@@ -0,0 +1,4 @@
+game:HttpGet("https://pastefy.app/iJ3URTft/raw")
+game:HttpGet("https://pastefy.app/iJ3URTft/raw")
+game:HttpGet("https://pandadevelopment.net/virtual/file/794b2f4a1b25653e")
+game:HttpGet("https://pandadevelopment.net/virtual/file/794b2f4a1b25653e")
